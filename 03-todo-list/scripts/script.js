55d2accd9b5b851fe4a055d2285a62bcda54fe0f
harshit-technocrat11 <harshit2z1agrawala@gmail.com>
2025-09-07 20:04:51 +0530

    defined all constants

---
+++ 03-todo-list/scripts/script.js
@@ -0,0 +1,19 @@
+
+//scripts
+const userInput = document.querySelector("#user-input");
+
+const addBtn = document.querySelector("#add-todo");
+
+
+const deleteBtn = document.querySelector("#delete-todo");
+console.log(deleteBtn);
+
+
+const todoText = document.querySelector("#todo-text")
+console.log(todoText);
+
+
+//utility functions
+
+
+
