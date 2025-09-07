a26af05027ae4aa5c7869d09e17667b8ebda0150
harshit-technocrat11 <harshit2z1agrawala@gmail.com>
2025-09-08 00:16:07 +0530

    creating - handleDelete()

--- 03-todo-list/database/database.js
+++ 03-todo-list/database/database.js
@@ -1,24 +1,24 @@
-let todos_list = [
-    {
-        task: "go to the gym"
-    },
-    {
-        task:"go to office"
-    },
-    {
-        task:"go to the cinema"
-    },
-    {
-        task:"walk your dog"
-    },
-    {
-        task:"go to market building - unit 1 , left lane, main building- buy some hotwheels"
-    },
-]
+// let original_todos = [
+//     {
+//         task: "go to the gym"
+//     },
+//     {
+//         task:"go to office"
+//     },
+//     {
+//         task:"go to the cinema"
+//     },
+//     {
+//         task:"walk your dog"
+//     },
+//     {
+//         task:"go to market building - unit 1 , left lane, main building- buy some hotwheels"
+//     },
+// ]
 
-todos_list = todos_list.map(task=>({
-    ...task,
-    id: Math.floor(Math.random()*100) + 1
-}))
+// todos_list = original_todos.map((task) => ({
+//   ...task,
+//   id: Math.floor(Math.random() * 100) + 1,
+// }));
 
-console.log(todos_list)
+// console.log(todos_list)
--- 03-todo-list/scripts/script.js
+++ 03-todo-list/scripts/script.js
@@ -1,13 +1,19 @@
+let todos_list = []
+// console.log(todos_list);
 
 let todoContainer = document.querySelector(".display-todos");
 
 //userinput
 let userInput = document.getElementById("user-input");
-console.log(userInput)
+console.log(userInput);
 
-console.log(todoContainer)
+const addBtn = document.getElementById("add-todo");
+console.log(addBtn);
 
-console.log(todos_list)
+const deleteBtn = document.getElementById("delete-todo");
+
+
+console.log(todoContainer);
 
 
 //utility functions
@@ -17,43 +23,57 @@ console.log(todos_list)
 //read - display
 
 function renderTodos(todos_list) {
-    todoContainer.innerHTML = "";  //clear it
-    //mapping
-    const todos = todos_list.map((todo)=>{
-        console.log(todo)
-
-        return `<div class="todo">  
-          <p class="todo-text">${todo.task}</p>
-          <button id="delete-todo" type="submit">
-            <i class="fa-solid fa-xmark" style="color: #dd0e0e"></i>
-          </button>
-        </div>`;
-    })
-
-    console.log(todos);
-    todoContainer.innerHTML = todos.join('')
-
+    todoContainer.innerHTML = ""
+  todos_list.map((obj) => {
+    let todoElem = document.createElement("div");
+    console.log(todoElem);
+    todoElem.classList.add("todo");
+    todoElem.innerHTML = `
+            <p class="todo-text">${obj.task}</p>
+            <button id="delete-todo" type="submit">
+                <i class="fa-solid fa-xmark" style="color: #dd0e0e"></i>
+           </button>
+    `;
+    //append to todo container
+    todoContainer.appendChild(todoElem);
+  });
 }
+
+//method -2  by creating elements
 
 //create - todo
-function createTodo(){
 
+function handleCreateTodo() {
+    if ( !userInput.value){
+        
+        return window.alert("task cannot be empty")
+    }
+  const text_input = userInput.value;
+  console.log(text_input)
+
+  const newtodo = {
+    task: text_input,
+    id: Date.now(),
+  };
+
+  console.log("new todo created:", newtodo.task, "\nid: ", newtodo.id)
+
+  todos_list.unshift(newtodo);
+
+  //render - after adding 
+  renderTodos(todos_list)
 }
 
+//delete todo
+function handleDeleteTodo(){
+    
+}
 
 //event listners
-userInput.addEventListener('input', (e)=>{
-    let newtodo = e.target.value;
-    console.log(newtodo)
-    todos_list.unshift(newtodo);
+addBtn.addEventListener("click", handleCreateTodo);
 
-})
+deleteBtn.addEventListener('click', handleDeleteTodo)
 
 
 //calling
-renderTodos(todos_list)
-
-
-
-
-
+renderTodos(todos_list);
